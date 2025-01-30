66bf4dee57b6738fc380aa3ca85dc8bddcde02ab
5dma <mark@lautman.net>
2025-01-29 22:00:12 -0500

    Add julian-time_t conversion

--- read_activity_list.c
+++ read_activity_list.c
@@ -2,6 +2,30 @@
 #include <headers.h>
 #include <unistd.h>
 
+time_t *get_time(int julian) {
+	int l = julian + 68569;
+	int n = 4 * l / 146097;
+	l = l - (146097 * n + 3) / 4;
+	int i = 4000 * (l + 1) / 1461001;
+	l = l - 1461 * i / 4 + 31;
+	int j = 80 * l / 2447;
+	int k = l - 2447 * j / 80;
+	l = j / 11;
+	j = j + 2 - 12 * l;
+	i = 100 * (n - 49) + i + l;
+	int year = i;
+	int month = j;
+	int day = k;
+	struct tm tm;
+	tm.tm_year = year - 1900;
+	tm.tm_mon = month - 1;
+	tm.tm_mday = day;
+
+	time_t *localtime = g_malloc(sizeof(time_t));
+	*localtime = mktime(&tm);
+	return localtime;
+}
+
 GSList *read_activity_list(Data_Passer *data_passer) {
 	if (access(data_passer->database_path, F_OK) != 0) {
 		g_print("The database %s does not exist. Try again.\n", data_passer->database_path);
@@ -25,12 +49,14 @@ GSList *read_activity_list(Data_Passer *data_passer) {
 	sqlite3_bind_int(stmt, 1, 2460703);
 	sqlite3_bind_int(stmt, 2, 1);
 	sqlite3_bind_int(stmt, 3, 2);
-	;
+
 
 	while (sqlite3_step(stmt) != SQLITE_DONE) {
 		for (int col = 0; col <= 4; col++) {
 			if (col == 0) {
-				printf("%d", sqlite3_column_int(stmt, col));
+				int julian = sqlite3_column_int(stmt, col);
+				get_time(julian);
+				printf("%d", julian);
 			} else {
 				printf("  %5.2f", sqlite3_column_double(stmt, col));
 			}
